607ea6475fd1e6618e7d809ba3ca164272523a7a
Slava <19uvs99@gmail.com>
2020-10-28 19:20:20 +0200

    fixes

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -5,21 +5,21 @@ import Logo from "../assets/crown.svg";
 import Bag from "../assets/shopping-bag.svg";
 
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img src={Logo} alt=""/>
-      </div>
-      <div className="options">
-        <div className="option"><span>SHOP</span></div>
-        <div className="option"><span>CONTACT</span></div>
-        <div className="option"><span>SIGN IN</span></div>
-        <div className="cart-icon">
-          <img className='shopping-icon' src={Bag} alt=""/>
-          <span className='item-count'>0</span>
+    return (
+        <div className="header">
+            <a href="#" className="logo-container">
+                <img src={Logo} alt=""/>
+            </a>
+            <nav className="options">
+                <a href="#" className="option"><span>SHOP</span></a>
+                <a href="#" className="option"><span>CONTACT</span></a>
+                <a href="#" className="option"><span>SIGN IN</span></a>
+                <a href="#" className="cart-icon">
+                    <img className='shopping-icon' src={Bag} alt=""/>
+                    <span className='item-count'>0</span>
+                </a>
+            </nav>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 export default Header;

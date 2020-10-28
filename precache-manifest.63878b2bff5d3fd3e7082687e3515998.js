25577127923c82a52f5713b9cdfb1fcbdfb6cf68
Slava <19uvs99@gmail.com>
2020-10-28 19:09:18 +0200

    Updates

---
+++ precache-manifest.63878b2bff5d3fd3e7082687e3515998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88a85a86fa10418e66d65c0a5282a27",
+    "url": "/Shop-react-SPA/index.html"
+  },
+  {
+    "revision": "7c66295a47baf5d3065b",
+    "url": "/Shop-react-SPA/static/css/main.0603c2b1.chunk.css"
+  },
+  {
+    "revision": "9c5f636157456dfa836e",
+    "url": "/Shop-react-SPA/static/js/2.be7ddbfc.chunk.js"
+  },
+  {
+    "revision": "7c66295a47baf5d3065b",
+    "url": "/Shop-react-SPA/static/js/main.7518bc15.chunk.js"
+  },
+  {
+    "revision": "6f17e6d2411703ff9b77",
+    "url": "/Shop-react-SPA/static/js/runtime~main.12aa270e.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Shop-react-SPA/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/Shop-react-SPA/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
